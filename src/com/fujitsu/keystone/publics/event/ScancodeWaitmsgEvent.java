ffb121bcf3532125fbe018a098f4fc987377b81a
Barrie <barrieshieh@gmail.com>
2015-11-19 22:41:01 +0800

    update

--- src/com/fujitsu/keystone/publics/event/ScancodeWaitmsgEvent.java
+++ src/com/fujitsu/keystone/publics/event/ScancodeWaitmsgEvent.java
@@ -44,35 +44,35 @@ public class ScancodeWaitmsgEvent extends Event {
 		 * 处理message 推送给用户的message
 		 * [QP02001,132020000001,AG,323232,2015年09月,2045年09月]气瓶安全云www.qpsafe.cn
 		 */
-		int lastIndex = scanResult.lastIndexOf("]");
-		// QP02001,132020000001,AG,323232,2015年09月,2045年09月
-		String tmp = scanResult.substring(1, lastIndex);
-		// [QP02001,132020000001,AG,323232,2015年09月,2045年09月]
-		String[] messArray = tmp.split(",");
-		StringBuffer buffer = new StringBuffer();
-		// 身份查询
-		if (MenuService.QP_SFCX.equals(requestJson.get(EVENT_KEY))) {
-			buffer.append("气瓶使用证编号:");
-			buffer.append(messArray[0]);
-			buffer.append(ENTER);
-			buffer.append("气瓶注册代码:");
-			buffer.append(messArray[1]);
-			buffer.append(ENTER);
-			buffer.append("气瓶充装单位(编号):");
-			buffer.append(messArray[3]);
-			buffer.append(ENTER);
-			buffer.append("气瓶编号:");
-			buffer.append(messArray[3]);
-			buffer.append(ENTER);
-			buffer.append("出厂日期:");
-			buffer.append(messArray[3]);
-			buffer.append(ENTER);
-			buffer.append("报废日期:");
-			buffer.append(messArray[3]);
-			buffer.append(ENTER);
-		}
-		message.setContent(buffer.toString());
-
+//		int lastIndex = scanResult.lastIndexOf("]");
+//		// QP02001,132020000001,AG,323232,2015年09月,2045年09月
+//		String tmp = scanResult.substring(1, lastIndex);
+//		// [QP02001,132020000001,AG,323232,2015年09月,2045年09月]
+//		String[] messArray = tmp.split(",");
+//		StringBuffer buffer = new StringBuffer();
+//		// 身份查询
+//		if (MenuService.QP_SFCX.equals(requestJson.get(EVENT_KEY))) {
+//			buffer.append("气瓶使用证编号:");
+//			buffer.append(messArray[0]);
+//			buffer.append(ENTER);
+//			buffer.append("气瓶注册代码:");
+//			buffer.append(messArray[1]);
+//			buffer.append(ENTER);
+//			buffer.append("气瓶充装单位(编号):");
+//			buffer.append(messArray[3]);
+//			buffer.append(ENTER);
+//			buffer.append("气瓶编号:");
+//			buffer.append(messArray[3]);
+//			buffer.append(ENTER);
+//			buffer.append("出厂日期:");
+//			buffer.append(messArray[3]);
+//			buffer.append(ENTER);
+//			buffer.append("报废日期:");
+//			buffer.append(messArray[3]);
+//			buffer.append(ENTER);
+//		}
+//		message.setContent(buffer.toString());
+		message.setContent(scanResult);
 		// 将消息对象转换成xml
 		respXml = MessageService.messageToXml(message);
 
