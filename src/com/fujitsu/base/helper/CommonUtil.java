6aceba7f5ddbd000b8aef1cb7105b456610e7e0b
Barrie <barrieshieh@gmail.com>
2015-11-21 17:28:08 +0800

    update

---
+++ src/com/fujitsu/base/helper/CommonUtil.java
@@ -0,0 +1,26 @@
+package com.fujitsu.base.helper;
+
+import java.util.Map;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+/**
+ * @author Administrator
+ * 
+ */
+public class CommonUtil {
+	/**
+	 * Map --> Bean 1: 利用Introspector,PropertyDescriptor实现 Map --> Bean
+	 * 
+	 * @param map
+	 * @param obj
+	 * @throws Exception
+	 */
+	public static Object transMap2Bean(Map<String, Object> map, Object obj)
+			throws Exception {
+		BeanUtils.populate(obj, map);
+		return obj;
+	}
+
+	
+}
