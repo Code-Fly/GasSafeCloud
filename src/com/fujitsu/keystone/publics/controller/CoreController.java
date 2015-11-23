f304d0558bc6563c9de2ad6d78479ec070289ff1
Barrie <barrieshieh@gmail.com>
2015-11-23 22:19:23 +0800

    add spring integration support

--- src/com/fujitsu/keystone/publics/controller/CoreController.java
+++ src/com/fujitsu/keystone/publics/controller/CoreController.java
@@ -1,17 +1,7 @@
 /**
- * 
+ *
  */
 package com.fujitsu.keystone.publics.controller;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import net.sf.json.JSONObject;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fujitsu.base.controller.BaseController;
 import com.fujitsu.base.exception.AccessTokenException;
@@ -20,78 +10,97 @@ import com.fujitsu.base.helper.FileUtil;
 import com.fujitsu.base.helper.KeystoneUtil;
 import com.fujitsu.base.helper.UrlUtil;
 import com.fujitsu.keystone.publics.service.impl.CoreService;
+import com.fujitsu.keystone.publics.service.impl.GreeterService;
+import net.sf.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Barrie
- *
  */
 @Controller
 @RequestMapping(value = "/api/keystone")
 public class CoreController extends BaseController {
 
-	@Resource
-	CoreService coreService;
+    @Resource
+    CoreService coreService;
 
-	@RequestMapping(value = "/core")
-	public void connect(HttpServletRequest request, HttpServletResponse response) {
-		String method = request.getMethod();
-		logger.info(method);
-		if ("GET".equals(method)) {
-			try {
-				coreService.doGet(request, response);
-			} catch (Exception e) {
-				logger.error(e.getMessage());
-			}
+    @Resource
+    GreeterService greeterService;
 
-		} else if ("POST".equals(method)) {
-			try {
-				coreService.doPost(request, response);
-			} catch (Exception e) {
-				logger.error(e.getMessage());
-			}
-		}
+    @RequestMapping(value = "/core")
+    public void connect(HttpServletRequest request, HttpServletResponse response) {
+        String method = request.getMethod();
+        logger.info(method);
+        if ("GET".equals(method)) {
+            try {
+                coreService.doGet(request, response);
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+            }
 
-	}
+        } else if ("POST".equals(method)) {
+            try {
+                coreService.doPost(request, response);
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+            }
+        }
 
-	@RequestMapping(value = "/token/refresh", produces = "application/json;charset=UTF-8")
-	@ResponseBody
-	public String refreshToken(HttpServletRequest request, HttpServletResponse response) throws ConnectionFailedException {
-		return KeystoneUtil.refreshLocalAccessToken().toString();
-	}
+    }
 
-	@RequestMapping(value = "/token/query", produces = "application/json;charset=UTF-8")
-	@ResponseBody
-	public String queryToken(HttpServletRequest request, HttpServletResponse response) throws ConnectionFailedException {
-		return KeystoneUtil.getLocalAccessToken().toString();
-	}
+    @RequestMapping(value = "/token/refresh", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String refreshToken(HttpServletRequest request, HttpServletResponse response) throws ConnectionFailedException {
 
-	@RequestMapping(value = "/jsapi/ticket/query", produces = "application/json;charset=UTF-8")
-	@ResponseBody
-	public String queryJsapiTicket(HttpServletRequest request, HttpServletResponse response) throws ConnectionFailedException, AccessTokenException {
-		JSONObject resp = coreService.getJsapiTicket(KeystoneUtil.getAccessToken());
-		return resp.getString("ticket");
-	}
+        greeterService.post("111111");
+        greeterService.post("222");
+        greeterService.post("333");
+        greeterService.get();
+        greeterService.get();
+        greeterService.get();
 
-	@RequestMapping(value = "/file/image/product")
-	@ResponseBody
-	public String getImage(HttpServletRequest request, HttpServletResponse response) {
-		String url = request.getParameter("url");
-		String pid = request.getParameter("pid");
-		if (null != url || null == pid) {
-			url = FileUtil.getWeChatImage(url, FileUtil.CATEGORY_PRODUCT, pid, false);
-		}
-		return url;
+        return KeystoneUtil.refreshLocalAccessToken().toString();
+    }
 
-	}
+    @RequestMapping(value = "/token/query", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String queryToken(HttpServletRequest request, HttpServletResponse response) throws ConnectionFailedException {
+        return KeystoneUtil.getLocalAccessToken().toString();
+    }
 
-	@RequestMapping(value = "/url/encode")
-	@ResponseBody
-	public String urlEncoder(HttpServletRequest request, HttpServletResponse response) {
-		String url = request.getParameter("url");
-		if (null != url) {
-			url = UrlUtil.toUTF8(url);
-		}
-		return url;
+    @RequestMapping(value = "/jsapi/ticket/query", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String queryJsapiTicket(HttpServletRequest request, HttpServletResponse response) throws ConnectionFailedException, AccessTokenException {
+        JSONObject resp = coreService.getJsapiTicket(KeystoneUtil.getAccessToken());
+        return resp.getString("ticket");
+    }
 
-	}
+    @RequestMapping(value = "/file/image/product")
+    @ResponseBody
+    public String getImage(HttpServletRequest request, HttpServletResponse response) {
+        String url = request.getParameter("url");
+        String pid = request.getParameter("pid");
+        if (null != url || null == pid) {
+            url = FileUtil.getWeChatImage(url, FileUtil.CATEGORY_PRODUCT, pid, false);
+        }
+        return url;
+
+    }
+
+    @RequestMapping(value = "/url/encode")
+    @ResponseBody
+    public String urlEncoder(HttpServletRequest request, HttpServletResponse response) {
+        String url = request.getParameter("url");
+        if (null != url) {
+            url = UrlUtil.toUTF8(url);
+        }
+        return url;
+
+    }
 }
