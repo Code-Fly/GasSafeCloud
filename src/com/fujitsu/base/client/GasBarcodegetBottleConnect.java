f6a9f2469fb7e324ebc916a09eb637890e34131d
partys88 <zhang_quanwei@yeah.net>
2015-11-25 21:48:51 +0800

    11

--- src/com/fujitsu/base/client/GasBarcodegetBottleConnect.java
+++ src/com/fujitsu/base/client/GasBarcodegetBottleConnect.java
@@ -15,8 +15,10 @@ import javax.websocket.WebSocketContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fujitsu.base.client.entity.BarcodegetBottleResMsg;
+import com.fujitsu.base.client.entity.BarcodegetBottleResult;
+
 import net.sf.json.JSONObject;
-import net.sf.json.JSONSerializer;
 import net.sf.json.JsonConfig;
 
 
@@ -31,7 +33,25 @@ public class GasBarcodegetBottleConnect {
 	
 	public  static Session session;
 	
-	
+
+    static {
+    	logger.info("start to connect " + uri);
+        WebSocketContainer container = null;
+        try {
+            container = ContainerProvider.getWebSocketContainer();
+        } catch (Exception ex) {
+        	logger.info("error" + ex);
+        }
+ 
+        try {
+            URI r = URI.create(uri);
+            session = container.connectToServer(GasBarcodegetBottleClient.class, r);
+        } catch (Exception e) {
+        	logger.error("connectToServer:"+uri,e);
+        }
+        logger.info("end to connect " + uri);
+    }
+    
 	public static void sendMsg(String msg){
 		logger.info("msg = " + msg);
 		try {
