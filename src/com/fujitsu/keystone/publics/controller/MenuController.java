824020d79cd2c8141b943f1a11833dff9d0e6941
Barrie <barrieshieh@gmail.com>
2015-12-05 19:13:53 +0800

    add get and delete menu api

--- src/com/fujitsu/keystone/publics/controller/MenuController.java
+++ src/com/fujitsu/keystone/publics/controller/MenuController.java
@@ -1,53 +1,70 @@
 /**
- * 
+ *
  */
 package com.fujitsu.keystone.publics.controller;
 
-import java.io.IOException;
+import com.fujitsu.base.constants.Const;
+import com.fujitsu.base.controller.BaseController;
+import com.fujitsu.base.exception.AccessTokenException;
+import com.fujitsu.base.exception.ConnectionFailedException;
+import com.fujitsu.base.helper.ConfigUtil;
+import com.fujitsu.base.helper.KeystoneUtil;
+import com.fujitsu.keystone.publics.service.iface.ICoreService;
+import com.fujitsu.keystone.publics.service.iface.IMenuService;
+import net.sf.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import net.sf.json.JSONObject;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.fujitsu.base.controller.BaseController;
-import com.fujitsu.base.exception.AccessTokenException;
-import com.fujitsu.base.exception.ConnectionFailedException;
-import com.fujitsu.base.helper.ConfigUtil;
-import com.fujitsu.base.constants.Const;
-import com.fujitsu.base.helper.KeystoneUtil;
-import com.fujitsu.keystone.publics.service.iface.ICoreService;
-import com.fujitsu.keystone.publics.service.iface.IMenuService;
+import java.io.IOException;
 
 /**
  * @author Barrie
- *
  */
 @Controller
 @RequestMapping(value = "/api/keystone")
 public class MenuController extends BaseController {
-	@Resource
-	IMenuService menuService;
-	@Resource
-	ICoreService coreService;
+    @Resource
+    IMenuService menuService;
+    @Resource
+    ICoreService coreService;
 
-	@RequestMapping(value = "/menu/create", produces = "application/json;charset=UTF-8")
-	@ResponseBody
-	public String create(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ConnectionFailedException, AccessTokenException {
-		String baseUrl = Const.MERCHANT_DOMAIN;
-		// 调用接口获取access_token
-		String at = KeystoneUtil.getAccessToken();
-		
-		String menuStr = ConfigUtil.getJson("menu.json");
-		
-		// 调用接口创建菜单
-		JSONObject resp = JSONObject.fromObject(menuService.create(at, JSONObject.fromObject(menuStr)));
-		return resp.toString();
-	}
+    @RequestMapping(value = "/menu/create", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String create(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ConnectionFailedException, AccessTokenException {
+        // 调用接口获取access_token
+        String at = KeystoneUtil.getAccessToken();
+
+        String menuStr = ConfigUtil.getJson("menu.json");
+
+        // 调用接口创建菜单
+        JSONObject resp = JSONObject.fromObject(menuService.create(at, JSONObject.fromObject(menuStr)));
+        return resp.toString();
+    }
+
+    @RequestMapping(value = "/menu/get", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String get(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ConnectionFailedException, AccessTokenException {
+        // 调用接口获取access_token
+        String at = KeystoneUtil.getAccessToken();
+
+        // 调用接口创建菜单
+        JSONObject resp = JSONObject.fromObject(menuService.get(at));
+        return resp.toString();
+    }
+
+    @RequestMapping(value = "/menu/delete", produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ConnectionFailedException, AccessTokenException {
+        // 调用接口获取access_token
+        String at = KeystoneUtil.getAccessToken();
+
+        // 调用接口创建菜单
+        JSONObject resp = JSONObject.fromObject(menuService.delete(at));
+        return resp.toString();
+    }
 }
